f10fd8e665fa31d2c6c89f346aed865cb4203c67
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-08-07 21:21:17 +0000

    Remove unused import for SupabaseDishCategories in CreateDishDto

--- shared/api/v1/dto/dishes/create-dish.dto.ts
+++ shared/api/v1/dto/dishes/create-dish.dto.ts
@@ -1,5 +1,4 @@
 import { IsString, IsUUID } from "@nestjs/class-validator";
-import { SupabaseDishCategories } from "shared/converters/convert_dish_categories";
 
 /** POST /v1/dishes のボディ */
 export class CreateDishDto {
