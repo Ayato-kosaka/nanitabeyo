f7bbd6a0585d223e7e30846291ae741eb4e78e64
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-08-19 18:49:15 +0000

    Refactor user response types to simplify structure and improve clarity

--- shared/api/v1/res/users.response.ts
+++ shared/api/v1/res/users.response.ts
@@ -1,27 +1,17 @@
-import { SupabaseDishMedia } from "../../../converters/convert_dish_media";
-import { SupabaseDishReviews } from "../../../converters/convert_dish_reviews";
-import { SupabaseRestaurants } from "../../../converters/convert_restaurants";
-import { SupabaseDishes } from "../../../converters/convert_dishes";
 import { SupabasePayouts } from "../../../converters/convert_payouts";
 import { SupabaseRestaurantBids } from "../../../converters/convert_restaurant_bids";
 import { SupabaseDishCategories } from "../../../converters/convert_dish_categories";
 import { PaginatedResponse } from "./paginated-response";
+import { DishMediaEntry } from "./dish-media.response";
 
 /** GET /v1/users/:id/dish-reviews のレスポンス型 */
 export type QueryUserDishReviewsResponse = PaginatedResponse<{
-	dish_media: SupabaseDishMedia;
-	dish_review: SupabaseDishReviews;
-	signedUrls: string[];
+	dishMediaEntry: DishMediaEntry;
 	hasMedia: boolean;
 }>;
 
 /** GET /v1/users/me/liked-dish-media のレスポンス型 */
-export type QueryMeLikedDishMediaResponse = PaginatedResponse<{
-	restaurant: SupabaseRestaurants;
-	dish: SupabaseDishes;
-	dish_media: SupabaseDishMedia;
-	dish_reviews: SupabaseDishReviews[];
-}>;
+export type QueryMeLikedDishMediaResponse = PaginatedResponse<DishMediaEntry>;
 
 /** GET /v1/users/me/payouts のレスポンス型 */
 export type QueryMePayoutsResponse = PaginatedResponse<SupabasePayouts>;
@@ -30,12 +20,9 @@ export type QueryMePayoutsResponse = PaginatedResponse<SupabasePayouts>;
 export type QueryMeRestaurantBidsResponse = PaginatedResponse<SupabaseRestaurantBids>;
 
 /** GET /v1/users/me/saved-dish-categories のレスポンス型 */
-export type QueryMeSavedDishCategoriesResponse = PaginatedResponse<SupabaseDishCategories>;
+export type QueryMeSavedDishCategoriesResponse = PaginatedResponse<SupabaseDishCategories & {
+	isSaved: boolean;
+}>;
 
 /** GET /v1/users/me/saved-dish-media のレスポンス型 */
-export type QueryMeSavedDishMediaResponse = PaginatedResponse<{
-	restaurant: SupabaseRestaurants;
-	dish: SupabaseDishes;
-	dish_media: SupabaseDishMedia;
-	dish_reviews: SupabaseDishReviews[];
-}>;
+export type QueryMeSavedDishMediaResponse = PaginatedResponse<DishMediaEntry>;
