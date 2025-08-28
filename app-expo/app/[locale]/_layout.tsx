e1164400f7970d9226b967b5ff88468fa546fead
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-08-28 12:13:48 +0000

    refactor: optimize useEffect dependencies in SearchScreen and RootLayout

--- app-expo/app/[locale]/_layout.tsx
+++ app-expo/app/[locale]/_layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout() {
 		}
 
 		i18n.locale = getResolvedLocale(locale);
-	}, [locale, scheme, router, logFrontendEvent]);
+	}, [locale]);
 
 	if (!fontsLoaded) return null;
 
