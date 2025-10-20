e8d39655b97385d3b6d2e3d430cda1c9f49d49f0
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-10-20 23:47:09 +0000

    #162 web でメディアプレビューが表示されない問題を解決 Fix InitialMediaPreview container height style

--- app-expo/features/map/components/InitialMediaPreview.tsx
+++ app-expo/features/map/components/InitialMediaPreview.tsx
@@ -131,6 +131,7 @@ export function InitialMediaPreview({ media }: InitialMediaPreviewProps) {
 const styles = StyleSheet.create({
 	container: {
 		width: "100%",
+		height: "100%",
 		marginBottom: 16,
 	},
 	mediaWrapper: {
