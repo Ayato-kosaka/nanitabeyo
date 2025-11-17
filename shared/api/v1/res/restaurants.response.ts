6e90768c87494db3689f68ec69b825816d0c0ba4
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-11-17 15:48:51 +0000

    #23 Refactor QueryRestaurantDishMediaResponse type to simplify structure

--- shared/api/v1/res/restaurants.response.ts
+++ shared/api/v1/res/restaurants.response.ts
@@ -31,14 +31,7 @@ export type CreateRestaurantResponse = RestaurantsEntity & {
 export type CreateRestaurantBidIntentResponse = { clientSecret: string };
 
 /** GET /v1/restaurants/:id/dish-media のレスポンス型 */
-export type QueryRestaurantDishMediaResponse = PaginatedResponse<
-	DishMediaEntry & {
-		dish: {
-			reviewCount: number;
-			averageRating: number;
-		};
-	}
->;
+export type QueryRestaurantDishMediaResponse = PaginatedResponse<DishMediaEntry>;
 
 /** GET /v1/restaurants/:id/restaurant-bids のレスポンス型 */
 export type QueryRestaurantBidsResponse = SupabaseRestaurantBids[];
