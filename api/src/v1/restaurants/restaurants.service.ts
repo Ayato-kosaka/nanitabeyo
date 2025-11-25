f7f681fe4a0b10149d60c73e5acccd472cbeba63
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-11-25 19:40:22 +0000

    format

--- api/src/v1/restaurants/restaurants.service.ts
+++ api/src/v1/restaurants/restaurants.service.ts
@@ -47,7 +47,7 @@ export class RestaurantsService {
     private readonly locationsService: LocationsService,
     private readonly cloudTasksService: CloudTasksService,
     private readonly storageService: StorageService,
-  ) { }
+  ) {}
 
   /* ------------------------------------------------------------------ */
   /*              GET /v1/restaurants/search (nearby restaurant search)               */
@@ -144,7 +144,7 @@ export class RestaurantsService {
       } catch (error) {
         throw new Error(
           'Failed to determine restaurant language code: ' +
-          (error as Error).message,
+            (error as Error).message,
         );
       }
 
@@ -274,15 +274,15 @@ export class RestaurantsService {
         ...convertPrismaToSupabase_Restaurants(restaurant),
         imageUrls: imageSignedUrl
           ? {
-            sm: imageSignedUrl,
-            md: imageSignedUrl,
-          }
+              sm: imageSignedUrl,
+              md: imageSignedUrl,
+            }
           : undefined,
       },
       meta: {
         ...restaurantReviewStats,
         ...restaurantBidStats,
-      }
+      },
     };
   }
 
