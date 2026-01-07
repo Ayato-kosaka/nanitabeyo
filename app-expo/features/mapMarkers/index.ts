8cecd637ced098f87b97b5bf86226ffc481f41ac
Ayato-kosaka <kosaka.ayato@gmail.com>
2026-01-07 18:31:22 +0000

    style: Format export statements for consistency in map marker entry point

--- app-expo/features/mapMarkers/index.ts
+++ app-expo/features/mapMarkers/index.ts
@@ -1,12 +1,12 @@
 // Map Marker 描画機能の公開エントリーポイント
 export {
-        MarkerBitmapRendererProvider,
-        useMarkerBitmapRenderer,
-        useMarkerBitmapState,
-        normalizeColor,
-        makeKey,
-        ACTIVE_COLOR_HEX,
-        INACTIVE_COLOR_HEX,
+	MarkerBitmapRendererProvider,
+	useMarkerBitmapRenderer,
+	useMarkerBitmapState,
+	normalizeColor,
+	makeKey,
+	ACTIVE_COLOR_HEX,
+	INACTIVE_COLOR_HEX,
 } from "./components/MarkerBitmapRendererProvider";
 export { AvatarBubbleMarkerBitmap } from "./components/AvatarBubbleMarkerBitmap";
 export { BubblePinBitmap } from "./components/BubblePinBitmap";
